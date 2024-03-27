3fcb6145f58c9f4a44e0d1f9382943f3a79361af
solomiia-marko <soliamark25@gmail.com>
2024-03-27 09:54:57 +0200

    Extended Mock Data

--- src/client/mockData.ts
+++ src/client/mockData.ts
@@ -1,30 +1,135 @@
 export const mockDataTasks = [
   {
     id: 1,
-    note: 'blablanbla',
+    note: 'Fix Alias',
     priority: 'low',
-    expiration_date: '25-04-2002',
-    tags: ['work']
+    expiration_date: '27-03-2024',
+    tags: ['internship']
   },
   {
     id: 2,
-    note: 'blablanbla',
+    note: 'Deploy Server',
     priority: 'medium',
-    expiration_date: '25-04-2002',
+    expiration_date: '27-03-2024',
     tags: ['fun']
   },
   {
     id: 3,
-    note: 'blablanbla',
+    note: 'Implement ability to create multiple tasks from one page',
     priority: 'hight',
-    expiration_date: '25-04-2002',
+    expiration_date: '28-03-2024',
     tags: ['work', 'fun']
   },
   {
     id: 4,
-    note: 'blablanbla',
+    note: 'Add Tags Form Fields',
     priority: 'critical',
-    expiration_date: '25-04-2002',
+    expiration_date: '28-03-2024',
     tags: ['study']
+  },
+  {
+    id: 5,
+    note: 'Setup Connection to the DB',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 6,
+    note: ' Implement GET all API',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 7,
+    note: 'Implement POST (create task) API',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 8,
+    note: 'Implement GET task (by id) API',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 9,
+    note: 'Implement PUT (update task) API',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 10,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 11,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['study']
+  },
+  {
+    id: 12,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 13,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 14,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 15,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 16,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 17,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 18,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
+  },
+  {
+    id: 19,
+    note: 'Bug Fixes',
+    priority: 'critical',
+    expiration_date: '27-03-2024',
+    tags: ['fun']
   }
 ];
--- src/server/index.ts
+++
@@ -1,4 +0,0 @@
-// const express = require('express');
-// const app = express();
-// const cors = require('cors');
-// app.listen(3002, () => console.log('Server successfully started'));
