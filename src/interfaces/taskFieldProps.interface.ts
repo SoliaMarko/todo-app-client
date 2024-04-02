1a1d697eb4c616afc2f207f4c6b412eda446d89a
solomiia-marko <soliamark25@gmail.com>
2024-04-02 14:11:02 +0300

    fix: set type assignments in form

--- src/interfaces/taskFieldProps.interface.ts
+++ src/interfaces/taskFieldProps.interface.ts
@@ -1,11 +1,10 @@
 import {PriorityOption} from './priority.interface';
-import {FormikProps} from 'formik';
-import {TaskForm} from './taskForm.interface';
 
 export interface TaskFieldProps {
   type?: string;
   options?: Array<PriorityOption>;
   name: string;
-  props: FormikProps<TaskForm>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  props: any;
   cols: number;
 }
