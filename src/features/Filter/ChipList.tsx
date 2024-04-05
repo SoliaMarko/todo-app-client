28b0082d336863ea404d9e35cde64117fd1b43b9
solomiia-marko <soliamark25@gmail.com>
2024-04-05 18:28:14 +0300

    fix: fixed import path for ChipListProps

--- src/features/Filter/ChipList.tsx
+++ src/features/Filter/ChipList.tsx
@@ -1,11 +1,11 @@
 import Chip from '@mui/material/Chip';
 import Stack from '@mui/material/Stack';
-import {ChipListProps} from '@/interfaces/chipProp.interface';
+import {ChipListProps} from '@/interfaces/featuresInterfaces/chipProp.interface';
 
 const ChipList = ({list}: ChipListProps) => {
   return (
     <Stack direction="row" spacing={1}>
-      {list.map((tag, i) => (
+      {list.map((tag: string, i: number) => (
         <Chip label={tag} variant="outlined" key={`${i}-${tag}`} />
       ))}
     </Stack>
