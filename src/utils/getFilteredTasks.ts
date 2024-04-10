36c14508b95e4a443a2af2750a06942b5af74974
solomiia-marko <soliamark25@gmail.com>
2024-04-10 14:53:07 +0300

    feat: implemented search

--- src/utils/getFilteredTasks.ts
+++ src/utils/getFilteredTasks.ts
@@ -2,17 +2,20 @@ import {Filters} from '@/interfaces/filterInterfaces/filters.props.interface';
 import {FormattedTaskRow} from '@/interfaces/formattedTaskRow.interface';
 import dayjs from 'dayjs';
 
-const filterByStatus = (tasks: FormattedTaskRow[], status: string[]) => {
+const findBySearch = (tasks: FormattedTaskRow[], search: string) => {
   return tasks.filter((task) => {
-    return status.includes(task.status);
+    let found = false;
+    Object.values(task).forEach((value) => {
+      if (`${value}`.toLowerCase().includes(search)) found = true;
+    });
+
+    return found;
   });
 };
 
-const filterByPriority = (tasks: FormattedTaskRow[], priority: string[]) => {
-  return tasks.filter((task) => {
-    return priority.includes(task.priority);
-  });
-};
+const filterByStatus = (tasks: FormattedTaskRow[], status: string[]) => tasks.filter((task) => status.includes(task.status));
+
+const filterByPriority = (tasks: FormattedTaskRow[], priority: string[]) => tasks.filter((task) => priority.includes(task.priority));
 
 const filterByDateRange = (task: FormattedTaskRow, from: Date, to: Date) => {
   const deadline = dayjs(task.deadline, 'DD-MM-YYYY');
@@ -23,10 +26,12 @@ const filterByDateRange = (task: FormattedTaskRow, from: Date, to: Date) => {
     const deadlineTs = deadline.valueOf();
 
     return deadlineTs >= min && deadlineTs <= max;
-  } else if (from) {
+  }
+  if (from) {
     const min = dayjs(from).valueOf();
     return deadline.valueOf() >= min;
-  } else if (to) {
+  }
+  if (to) {
     const max = dayjs(to).valueOf();
     return deadline.valueOf() <= max;
   }
@@ -37,18 +42,17 @@ const filterByDateRange = (task: FormattedTaskRow, from: Date, to: Date) => {
 const getFilteredTasks = (tasks: FormattedTaskRow[], filters: Filters) => {
   let filteredTasks = [...tasks];
 
+  if (filters.search) {
+    filteredTasks = findBySearch(filteredTasks, filters.search);
+  }
   if (filters.status.length) {
     filteredTasks = filterByStatus(filteredTasks, filters.status);
   }
-
   if (filters.priority.length) {
     filteredTasks = filterByPriority(filteredTasks, filters.priority);
   }
-
   if (filters.from || filters.to) {
-    filteredTasks = filteredTasks.filter((task) => {
-      return filterByDateRange(task, filters.from, filters.to);
-    });
+    filteredTasks = filteredTasks.filter((task) => filterByDateRange(task, filters.from, filters.to));
   }
 
   return filteredTasks;
